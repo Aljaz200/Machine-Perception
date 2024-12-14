9b5976ebedd7877a3a978762edb2ebd88339446f
Aljaž <aljaz03so@gmail.com>
2024-12-14 23:34:38 +0100

    Add readme file

---
+++ README.md
@@ -0,0 +1,13 @@
+# Umetno zaznavanje
+Delo pri predmetu Umetno zaznavanje (python) UNI, Fakulteta za računalništvo in informatiko, Univerza v Ljubljani, 2023/24:
+
+- rešene domače naloge,
+- python skript za pretvorbo Jupyter notebookov v python skripte.
+
+Seznam rešenih domačih nalog:
+----------
+- Assignment 1: Basic image processing and histograms
+- Assignment 2: Recognition using histograms, convolution, and image filtering
+- Assignment 3: Image segmentation and object detection
+- Assignment 4: Feature points, matching, homography
+- Assignment 5: Epipolar geometry and triangulation
--- convert.py
+++ convert.py
@@ -1,7 +1,9 @@
 from nbconvert import PythonExporter
 
+# ukaz v cmd: jupyter nbconvert --to script ***.ipynb
+
 # Pot do notebooka
-notebook_filename = 'ass4.ipynb'
+notebook_filename = input('Vnesite pot do notebooka: ')
 
 # Ustvarite PythonExporter
 exporter = PythonExporter()
@@ -9,6 +11,8 @@ exporter = PythonExporter()
 # Izvozi notebook kot Python skripto
 body, resources = exporter.from_filename(notebook_filename)
 
+python_filename = notebook_filename.replace('.ipynb', '.py')
+
 # Shranite Python skripto
-with open('ass4.py', 'w') as f:
+with open(python_filename, 'w') as f:
     f.write(body)
